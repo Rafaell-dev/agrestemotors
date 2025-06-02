2f3a14802575dce7d318d568406229b57bc96aa9
rafaelldev <rafaelaraujo.contact@gmail.com>
2025-06-01 22:38:21 -0300

    feat: ajustes de estilo

--- public/js/header.js
+++ public/js/header.js
@@ -3,7 +3,9 @@ export const header = (() => {
 
     const headerContainer = /*HTML*/`
         <div class="container">
-            <img src="./assets/logo_agreste_branca.png" alt="Logo branca Agreste Motors">
+            <a href="./index.html" class="logo">
+                <img src="./assets/logo_agreste_branca.png" alt="Logo branca Agreste Motors">
+            </a>
 
             <button class="hamburger-menu"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
