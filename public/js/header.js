2ae48e78aeca9334534db029e9bed5fc8de34af4
rafaelldev <rafaelaraujo.contact@gmail.com>
2025-06-03 01:03:04 -0300

    feat: implementando carousel

--- public/js/header.js
+++ public/js/header.js
@@ -4,7 +4,7 @@ export const header = (() => {
     const headerContainer = /*HTML*/`
         <div class="container">
             <a href="./index.html" class="logo">
-                <img src="./assets/logo_agreste_branca.png" alt="Logo branca Agreste Motors">
+            <img src="./assets/logo_agreste_branca.png" alt="Logo branca Agreste Motors">
             </a>
 
             <button class="hamburger-menu"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
